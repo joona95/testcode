0fd5dcccfa05cb7d128395b68d7fc5bcdc79369f
joona95 <joona95@naver.com>
2023-07-20 22:59:42 +0900

    refactor: 입력받은 값을 요청값으로 바꾸는 클래스 추가

--- src/main/java/com/example/testcode/TestcodeApplication.java
+++ src/main/java/com/example/testcode/TestcodeApplication.java
@@ -1,15 +1,14 @@
 package com.example.testcode;
 
-import java.util.Scanner;
-
 public class TestcodeApplication {
 
     public static void main(String[] args) {
         String[] parts = new CalculationRequestReader().read();
-        long num1 = Long.parseLong(parts[0]);
-        long num2 = Long.parseLong(parts[2]);
-        String operator = parts[1];
-        long answer = new Calculator().calculate(num1, operator, num2);
+        CalculationRequest calculationRequest = new CalculationRequest(parts);
+        long answer = new Calculator().calculate(
+                calculationRequest.getNum1(),
+                calculationRequest.getOperator(),
+                calculationRequest.getNum2());
         System.out.println(answer);
     }
 
