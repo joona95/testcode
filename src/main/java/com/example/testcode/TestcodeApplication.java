d25fbabfe4897881c473d28a09baae13e1226c83
joona95 <joona95@naver.com>
2023-07-20 23:02:36 +0900

    refactor: 입력받는 메서드 리턴값을 구조체로 변경

--- src/main/java/com/example/testcode/TestcodeApplication.java
+++ src/main/java/com/example/testcode/TestcodeApplication.java
@@ -3,8 +3,7 @@ package com.example.testcode;
 public class TestcodeApplication {
 
     public static void main(String[] args) {
-        String[] parts = new CalculationRequestReader().read();
-        CalculationRequest calculationRequest = new CalculationRequest(parts);
+        CalculationRequest calculationRequest = new CalculationRequestReader().read();
         long answer = new Calculator().calculate(
                 calculationRequest.getNum1(),
                 calculationRequest.getOperator(),
